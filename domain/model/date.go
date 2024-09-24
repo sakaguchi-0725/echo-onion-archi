112015bdea347c987b77fe2460669be016ddd0e3
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2024-09-24 14:18:45 +0900

    DateオブジェクトにStringメソッドを追加

--- domain/model/date.go
+++ domain/model/date.go
@@ -8,3 +8,8 @@ func NewDate(t time.Time) Date {
 	jst := time.FixedZone("Asia/Tokyo", 9*60*60)
 	return Date(time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, jst))
 }
+
+func (d Date) String() string {
+	t := time.Time(d)
+	return t.Format("2006-01-02")
+}
