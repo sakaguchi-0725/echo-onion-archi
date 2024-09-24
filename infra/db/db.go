37c14b3c47184ae0af3126c77c784312849c72a5
Kazuma Sakaguchi <sakaguchi@KazumanoMacBook-Pro.local>
2024-09-24 22:01:54 +0900

    db接続処理実装

---
+++ infra/db/db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+
+	"github.com/sakaguchi-0725/echo-onion-arch/infra/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB(config *config.DBConfig) (*gorm.DB, error) {
+	dsn := config.DSN()
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to the database!")
+	return db, nil
+}
---
+++ infra/db/test_db.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"log"
+
+	"github.com/sakaguchi-0725/echo-onion-arch/infra/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewTestDB(config *config.TestDBConfig) (*gorm.DB, error) {
+	dsn := config.DSN()
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("Successfully connected to the test database!")
+	return db, nil
+}
